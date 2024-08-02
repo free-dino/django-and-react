5481a880b8e2f45ea22b14430bad8ed3840e884b
sangtran2222004 <sang2222004@gmail.com>
2024-08-02 15:46:19 +0700

    change

--- django-api/CoreRoot/settings.py
+++ django-api/CoreRoot/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # local apps
     "core",
+    'core.user'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'core_user.User'
--- django-api/core/user/models.py
+++ django-api/core/user/models.py
@@ -10,6 +10,47 @@ from django.db import models
 from django.http import Http404
 
 
+class UserManager(BaseUserManager):
+    def get_object_by_public_id(self, public_id):
+        try:
+            instance = self.get(public_id=public_id)
+            return instance
+        except (ObjectDoesNotExist, ValueError, TypeError):
+            return Http404
+
+    def create_user(self, username, email, password=None, **kwargs):
+        """Create and return a 'User' with a username, email and password."""
+        if username is None:
+            raise TypeError("User must have a username")
+        if email is None:
+            raise TypeError("User must have an email")
+        if password is None:
+            raise TypeError("User must have a password")
+
+        user = self.model(
+            username=username, email=self.normalize_email(email), **kwargs
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+
+    def create_superuser(self, username, email, password, **kwargs):
+        """Create and return an "User" with superuser (admin) permission"""
+        if password is None:
+            raise TypeError("Superusers must have a password")
+        if email is None:
+            raise TypeError("Superusers must have an email")
+        if username is None:
+            raise TypeError("Superusers must have a username")
+        
+        user = self.create_superuser(username, email, password, **kwargs)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     public_id = models.UUIDField(
         db_index=True, unique=True, default=uuid.uuid4, editable=False
@@ -24,16 +65,16 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_superuser = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now_add=True)
-    
-    
+
     # when log in, you will have to use email instead of username
-    USERNAME_FIELD = 'user'
-    EMAIL_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    objects = UserManager()
+
     def __str__(self) -> str:
         return f"{self.email}"
-    
+
     @property
     def name(self) -> str:
         return f"{self.first_name} {self.last_name}"
